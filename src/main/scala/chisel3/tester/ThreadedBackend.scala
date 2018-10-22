e234401e3977b76f26320c40933057718d2e8159
ducky <richard.lin@berkeley.edu>
2018-10-22 11:35:31 -0700

    Add signal override links

--- src/main/scala/chisel3/tester/ThreadedBackend.scala
+++ src/main/scala/chisel3/tester/ThreadedBackend.scala
@@ -33,6 +33,7 @@ trait ThreadedBackend {
   // Root timescope, cannot be modified, with no signals poked.
   class RootTimescope extends BaseTimescope {
     override def threadOption = None
+    val overridingPokes = mutable.HashMap[Data, mutable.ListBuffer[Timescope]]()
   }
 
   // Timescope that is the root of a thread, contains no signals but can have a parent
@@ -54,19 +55,16 @@ trait ThreadedBackend {
 
     override def threadOption = parentTimescope.threadOption
 
-    // Latest poke on a signal in this timescope
-    val pokes = mutable.HashMap[Data, PokeRecord]()  // Latest poke on each data
-    // TODO: do we need to record earlier pokes, to differentiate new pokes?
+    // All pokes on a signal in this timescope, ordered from first to last
+    // TODO: can we get away with just first and most recent?
+    val pokes = mutable.HashMap[Data, mutable.ListBuffer[PokeRecord]]()  // Latest poke on each data
+    // List of timescopes overriding a signal, including ones closed / reverted on this timestep
+    val overridingPokes = mutable.HashMap[Data, mutable.ListBuffer[Timescope]]()
   }
 
   //
   // Threading checking data structures and utilities
   //
-
-  // List of the top Timescope for every Data object.
-  // During the testdriver phase, new timescopes may be added that supersede previous ones, but
-  // none are to be removed until during the checking phase.
-  val activePokes = mutable.HashMap[Data, mutable.ListBuffer[Timescope]]()
 
   case class PeekRecord(timescope: Timescope, timestep: Int, actionId: Int, trace: Throwable)
   // Active peeks on a signal, instantaneous on the current timestep
@@ -79,16 +77,49 @@ trait ThreadedBackend {
    */
   def doPoke(signal: Data, value: BigInt, trace: Throwable): Unit = {
     val timescope = currentThread.get.getTimescope
+    // On the first poke, add a link from the previous timescope
+    if (!timescope.pokes.contains(signal)) {
+      /**
+       * Return if the timescope modified signal before childTimescope spawned
+       * (or, if childTimescope considers timescope a parent wrt the signal)
+       */
+      def timescopeContainsThreadSignal(signal: Data,
+          timescope: Timescope, childTimescope: ThreadRootTimescope): Boolean = {
+        timescope.pokes.get(signal) match {
+          case None => false
+          case Some(pokeRecords) => pokeRecords.head.actionId < childTimescope.parentActionId
+        }
+      }
+
+      def getContainingTimescope(signal: Data,
+          timescope: BaseTimescope, childTimescope: BaseTimescope): BaseTimescope = {
+        (timescope, childTimescope) match {
+          case (timescope: RootTimescope, _) => timescope
+          case (timescope: Timescope, _: Timescope) if timescope.pokes.contains(signal) => timescope
+          case (timescope: Timescope, childTimescope: ThreadRootTimescope)
+              if timescopeContainsThreadSignal(signal, timescope, childTimescope) => timescope
+          // TODO: dedup above
+          case (timescope: Timescope, _) => getContainingTimescope(signal, timescope.parentTimescope, timescope)
+          case (timescope: ThreadRootTimescope, _) => getContainingTimescope(signal, timescope.parentTimescope, timescope)
+        }
+      }
+
+      // TODO dedup with better type hierarchy
+      getContainingTimescope(signal, timescope.parentTimescope, timescope) match {
+        case containingTimescope: RootTimescope =>
+          containingTimescope.overridingPokes.getOrElseUpdate(signal, mutable.ListBuffer[Timescope]()).append(
+            timescope)
+        case containingTimescope: Timescope =>
+          containingTimescope.overridingPokes.getOrElseUpdate(signal, mutable.ListBuffer[Timescope]()).append(
+            timescope)
+        case _ => require(false)
+      }
+    }
+
     // Update the timescope
-    timescope.pokes.put(signal,
+    timescope.pokes.getOrElseUpdate(signal, mutable.ListBuffer[PokeRecord]()).append(
         PokeRecord(currentTimestep, timescope.nextActionId, value, trace))
     timescope.nextActionId += 1
-
-    // Update the global active pokes list
-    val signalActivePokes = activePokes.getOrElseUpdate(signal, mutable.ListBuffer())
-    if (!signalActivePokes.contains(timescope)) {
-      signalActivePokes += timescope
-    }
   }
 
   /**
@@ -127,13 +158,13 @@ trait ThreadedBackend {
     currentThread.get.topTimescope = timescope.parentTimescope
 
     // Build a revert map of poked signals
-    timescope.pokes.map { case (data, pokeRecord) =>
+    timescope.pokes.map { case (data, _) =>
       def getPreviousPoke(startingTimescope: BaseTimescope, signal: Data): Option[PokeRecord] = {
         startingTimescope match {
           case _: RootTimescope => None
           case startingTimescope: ThreadRootTimescope => getPreviousPoke(startingTimescope.parentTimescope, signal)
           case startingTimescope: Timescope => startingTimescope.pokes.get(signal) match {
-            case Some(pokeRecord) => Some(pokeRecord)
+            case Some(pokeRecord) => Some(pokeRecord.last)
             case None => getPreviousPoke(startingTimescope.parentTimescope, signal)
           }
         }
