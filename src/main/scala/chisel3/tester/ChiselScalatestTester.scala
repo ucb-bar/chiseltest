e85fbad634599969069ce7595a72c8caeeb5af2e
Chick Markley <chick@qrhino.com>
2019-03-09 15:14:21 -0800

    Provide support for a way to use testers2 in jupyter notebook (#22)
    * Provide support for a way to use testers2 in jupyter notebook
    - Removes requirement that tests be run within a scalatest context
    - New Entry point ChiselTester.apply creates and runs a test
    - Create ChiselTesterHelper for shared code existing and new ways to run
    
    * Per suggestions by ducky
    - now can invoke non scalatest test the same as the scalatest way
      - import chisel3.tester.ChiselTester.test
      - then just use `test(dutgen) { c => }`
      - one example test changed to illustrate this way
    - Got rid of ChiselTesterHelper
      - Adding functions from it as default implementations into BackendInterface
    - when using simple tester it creates a new test directory for every test
      - s"chisel_test_${System.currentTimeMillis()}_$getCount"
    
    * Per round 2 suggestions by ducky
    - Add FailedExpectException to eliminate dependence on scalatest, but more specific that Exception
    - Rename ChiselTester to RawTester
    - Eliminate count from RawTesters, probably overkill on unique naming
    
    * Per round 3 suggestions by ducky
    - Can now throw TestFailedException when being used in scalatest
    -

--- src/main/scala/chisel3/tester/ChiselScalatestTester.scala
+++ src/main/scala/chisel3/tester/ChiselScalatestTester.scala
@@ -2,83 +2,33 @@
 
 package chisel3.tester
 
-import scala.collection.mutable
-import scala.util.DynamicVariable
-
+import chisel3.experimental.MultiIOModule
+import firrtl.ExecutionOptionsManager
 import org.scalatest._
 import org.scalatest.exceptions.TestFailedException
 
-import firrtl.ExecutionOptionsManager
-import chisel3._
-import chisel3.experimental.MultiIOModule
+import scala.util.DynamicVariable
 
 trait ChiselScalatestTester extends Assertions with TestSuiteMixin with TestEnvInterface { this: TestSuite =>
   // Provide test fixture data as part of 'global' context during test runs
   protected var scalaTestContext = new DynamicVariable[Option[NoArgTest]](None)
 
   abstract override def withFixture(test: NoArgTest): Outcome = {
-    require(scalaTestContext.value == None)
+    require(scalaTestContext.value.isEmpty)
     scalaTestContext.withValue(Some(test)) {
       super.withFixture(test)
     }
   }
 
-  protected val batchedFailures = mutable.ArrayBuffer[TestFailedException]()
+  override val useTestFailedException = true
 
   // Stack trace data to help generate more informative (and localizable) failure messages
-  protected var topFileName: Option[String] = None  // best guess at the testdriver top filename
-
-  override def testerFail(msg: String): Unit = {
-    batchedFailures += new TestFailedException(s"$msg", 4)
-  }
-
-  protected def getExpectDetailedTrace(trace: Seq[StackTraceElement], inFile: String): String = {
-    val fullTrace = Context().backend.getParentTraceElements ++ trace
-
-    // In the threading case, this needs to be overridden to trace through parent threads
-    val lineNumbers = fullTrace.collect {
-      case ste if ste.getFileName == inFile => ste.getLineNumber
-    }.mkString(", ")
-    if (lineNumbers.isEmpty()) {
-      s" (no lines in $inFile)"
-    } else {
-      s" (lines in $inFile: $lineNumbers)"
-    }
-  }
-
-  override def testerExpect(expected: Any, actual: Any, signal: String, msg: Option[String]): Unit = {
-    if (expected != actual) {
-      val appendMsg = msg match {
-        case Some(msg) => s": $msg"
-        case _ => ""
-      }
-
-      val trace = new Throwable
-      val expectStackDepth = trace.getStackTrace.indexWhere(ste =>
-        ste.getClassName == "chisel3.tester.package$testableData" && ste.getMethodName == "expect")
-      require(expectStackDepth != -1,
-        s"Failed to find expect in stack trace:\r\n${trace.getStackTrace.mkString("\r\n")}")
-
-      val trimmedTrace = trace.getStackTrace.drop(expectStackDepth + 2)
-      val detailedTrace = topFileName.map(getExpectDetailedTrace(trimmedTrace.toSeq, _)).getOrElse("")
-
-      batchedFailures += new TestFailedException(
-          s"$signal=$actual did not equal expected=$expected$appendMsg$detailedTrace",
-          expectStackDepth + 1)
-    }
-  }
-
-  override def checkpoint(): Unit = {
-    // TODO: report multiple exceptions simultaneously
-    for (failure <- batchedFailures) {
-      throw failure
-    }
-  }
+  var topFileName: Option[String] = None  // best guess at the testdriver top filename
 
   private def runTest[T <: MultiIOModule](tester: BackendInstance[T])(testFn: T => Unit) {
     // Try and get the user's top-level test filename
     val internalFiles = Set("ChiselScalatestTester.scala", "BackendInterface.scala")
-    val topFileNameGuess = (new Throwable).getStackTrace.apply(3).getFileName()
+    val topFileNameGuess = (new Throwable).getStackTrace.apply(3).getFileName
     if (internalFiles.contains(topFileNameGuess)) {
       println("Unable to guess top-level testdriver filename from stack trace")
       topFileName = None
@@ -91,17 +41,17 @@ trait ChiselScalatestTester extends Assertions with TestSuiteMixin with TestEnvI
     Context.run(tester, this, testFn)
   }
 
-  def getTestOptions(): TesterOptions = {
+  def getTestOptions: TesterOptions = {
     val test = scalaTestContext.value.get
     TesterOptions(test.name, test.configMap.contains("writeVcd"))
   }
 
   // This should be the only user-called function
   def test[T <: MultiIOModule](dutGen: => T)(testFn: T => Unit) {
-    runTest(Context.createDefaultTester(() => dutGen, getTestOptions(), None))(testFn)
+    runTest(Context.createDefaultTester(() => dutGen, getTestOptions, None))(testFn)
   }
 
   def test[T <: MultiIOModule](dutGen: => T, execOptions: ExecutionOptionsManager)(testFn: T => Unit) {
-    runTest(Context.createDefaultTester(() => dutGen, getTestOptions(), Some(execOptions)))(testFn)
+    runTest(Context.createDefaultTester(() => dutGen, getTestOptions, Some(execOptions)))(testFn)
   }
 }
