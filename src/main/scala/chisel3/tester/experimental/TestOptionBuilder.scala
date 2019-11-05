7609dca9939a6aeceaddc32491e0031b5c8be188
Chick Markley <chick@qrhino.com>
2019-11-04 16:40:31 -0800

    Fix toolchain so treadle supports boringutils (#79)
    * Fix toolchain so treadle supports boringutils
    - Add the low firrtl compiler to TreadleExecutive
      - Send hint to treadle so it knows compiled form
    - Fix backward compatibility with Driver
      - Check compiler type to help figure out the backend
      - Backward compatibility only supported for treadle and verilator
    - Add boringutils test
    - Backward compatibilty requires compile workaround
      - Can we just eliminate Driver compatibility mode here???
    - Fixed CopyVpiFiles
      - looks like this might fail if early file was there but a late one wasn't
      - this code came from chisel-testers
    - Fix top level test names
      - behavior of "Testers2 with Vcs"
      - behavior of "Testers2 with Verilator"
    - Shortened VCS to just see if a simple circuit will build and simulate.
      - in long run we still need mechanism to run an arbitrary test with arbitrary backend
    
    * - Made associations between compiler and backend in compat mode explicit
    - In BoreTest
      - removed unused code.
    - In MixedVec
      - Renamed to VecPokeExpectTest
      - tested all addresses
      - removed bundle
    
    (cherry picked from commit dd6d083bb68f7ee75f5b25eddbb68cd54095d976)

--- src/main/scala/chisel3/tester/experimental/TestOptionBuilder.scala
+++ src/main/scala/chisel3/tester/experimental/TestOptionBuilder.scala
@@ -4,8 +4,12 @@ package chisel3.tester.experimental
 
 import chisel3._
 import chisel3.tester._
-import firrtl.{AnnotationSeq, ExecutionOptionsManager}
+import chisel3.tester.internal.{TreadleBackendAnnotation, VerilatorBackendAnnotation}
 import treadle.HasTreadleSuite
+import firrtl.stage.CompilerAnnotation
+import firrtl.{
+  AnnotationSeq, ExecutionOptionsManager, LowFirrtlCompiler, MinimumVerilogCompiler,
+  NoneCompiler, SystemVerilogCompiler, VerilogCompiler}
 
 package object TestOptionBuilder {
   implicit class ChiselScalatestOptionBuilder[T <: MultiIOModule](x: ChiselScalatestTester#TestBuilder[T]) {
@@ -15,7 +19,16 @@ package object TestOptionBuilder {
 
     @deprecated("Use withAnnotations instead")
     def withExecOptions(manager: ExecutionOptionsManager with HasTreadleSuite): ChiselScalatestTester#TestBuilder[T] = {
-      new x.outer.TestBuilder[T](x.dutGen, manager.toAnnotationSeq)
+      val annos = manager.toAnnotationSeq.map {
+        case CompilerAnnotation(compiler) if compiler.isInstanceOf[LowFirrtlCompiler]      => TreadleBackendAnnotation
+        case CompilerAnnotation(compiler) if compiler.isInstanceOf[NoneCompiler]           => TreadleBackendAnnotation
+        case CompilerAnnotation(compiler) if compiler.isInstanceOf[VerilogCompiler]        => VerilatorBackendAnnotation
+        case CompilerAnnotation(compiler) if compiler.isInstanceOf[MinimumVerilogCompiler] => VerilatorBackendAnnotation
+        case CompilerAnnotation(compiler) if compiler.isInstanceOf[SystemVerilogCompiler]  => VerilatorBackendAnnotation
+        case anno => anno
+      }
+
+      new x.outer.TestBuilder[T](x.dutGen, annos)
     }
 
     @deprecated("Use withAnnotations instead")
