877cdf0b31fb6ba926b1c9a50503ceecd4b17007
Jiuyang liu <liujiuyang1994@gmail.com>
2020-10-19 04:53:58 +0000

    remove TestEnvInterface.

--- src/main/scala/chiseltest/internal/Testers2.scala
+++ src/main/scala/chiseltest/internal/Testers2.scala
@@ -9,13 +9,13 @@ import firrtl.stage.phases.DriverCompatibility.TopNameAnnotation
 import scala.util.DynamicVariable
 
 object Context {
-  class Instance(val backend: BackendInterface, val env: TestEnvInterface) {}
+  class Instance(val backend: BackendInterface) {}
 
   private var context = new DynamicVariable[Option[Instance]](None)
 
-  def run[T <: MultiIOModule](backend: BackendInstance[T], env: TestEnvInterface, testFn: T => Unit) {
+  def run[T <: MultiIOModule](backend: BackendInstance[T], testFn: T => Unit) {
     require(context.value.isEmpty)
-    context.withValue(Some(new Instance(backend, env))) {
+    context.withValue(Some(new Instance(backend))) {
       backend.run(testFn)
     }
   }
