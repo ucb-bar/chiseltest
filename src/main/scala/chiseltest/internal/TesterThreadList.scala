8eae3a4946297117574b10151f1ec098bd779a85
Jiuyang liu <liujiuyang1994@gmail.com>
2020-10-19 04:50:50 +0000

    split TesterThreadList to file.

---
+++ src/main/scala/chiseltest/internal/TesterThreadList.scala
@@ -0,0 +1,23 @@
+package chiseltest.internal
+
+import chisel3.Clock
+
+trait AbstractTesterThread
+
+class TesterThreadList(protected val elts: Seq[AbstractTesterThread]) {
+  def toSeq(): Seq[AbstractTesterThread] = elts
+
+  def join() {
+    Context().backend.doJoin(elts, None)
+  }
+
+  def joinAndStep(clock: Clock) {
+    Context().backend.doJoin(elts, Some(clock))
+  }
+
+  def ++(others: TesterThreadList): TesterThreadList = {
+    new TesterThreadList(elts ++ others.elts)
+  }
+
+  val fork: ForkBuilder = new ForkBuilder(None, None, elts)
+}
