9e9b1c314195760a35434504afc857ab030fdd90
Jiuyang liu <liujiuyang1994@gmail.com>
2021-01-06 01:51:30 +0000

    add Documentation to ChiselTestOptions.

--- src/main/scala/chiseltest/stage/ChiselTestOptions.scala
+++ src/main/scala/chiseltest/stage/ChiselTestOptions.scala
@@ -7,6 +7,27 @@ import chiseltest.ChiselTestException
 import chiseltest.backends.{SimulatorBackend, SimulatorInterface}
 import firrtl.ir.Port
 
+/** [[firrtl.AnnotationSeq]] viewer for [[ChiselTestPhase]].
+  * It contains data structures which will be used at different phases.
+  *
+  * @param circuit extract [[firrtl.ir.Circuit]] from [[firrtl.stage.FirrtlCircuitAnnotation]] and [[treadle.TreadleCircuitStateAnnotation]]
+  * @param topName extract [[firrtl.ir.Circuit.main]] from [[firrtl.stage.FirrtlCircuitAnnotation]] and [[treadle.TreadleCircuitStateAnnotation]]
+  * @param backend extract [[SimulatorBackend]] from [[TreadleBackendAnnotation]], [[VerilatorBackendAnnotation]], [[VcsBackendAnnotation]]
+  * @param simulatorCFlags extract arguments for C compiler in verilator or vcs backend from [[SimulatorCFlagsAnnotation]]
+  * @param simulatorFlags extract arguments for verilator or vcs program from [[SimulatorFlagsAnnotation]]
+  * @param targetDir extract extract path string which stores simulation files from [[firrtl.options.TargetDirAnnotation]]
+  * @param commands extract commands string to run generated simulation binary if exists from [[TestCommandAnnotation]]
+  * @param waveForm extract waveform suffix string from [[WriteVcdAnnotation]],
+  * @param dut extract [[chisel3.Module]] top instance from [[chisel3.stage.DesignAnnotation]].
+  * @param testFunction extract function to test dut from [[TestFunctionAnnotation]]
+  * @param simulatorInterface extract [[SimulatorInterface]] for a specific backend from [[chiseltest.backends.SimulatorInterfaceAnnotation]]
+  * @param topPorts extract all ports name of top from [[treadle.TreadleCircuitStateAnnotation]] or [[firrtl.stage.FirrtlCircuitAnnotation]]
+  * @param topPortsNameMap extract a HashMap: Data -> String from [[chiseltest.stage.phases.ExportedSingalsAnnotation]]
+  *                        @todo Shoulde be refactored to [[firrtl.annotations.Target]] for better namespace consistency.
+  * @param topCombinationalPaths extract combinational path on the top Ports in Data -> Data from [[chiseltest.stage.phases.TopCombinationalPathAnnotation]]
+  * @param coverageAnnotations extract [[LineCoverageAnnotation]] [[ToggleCoverageAnnotation]] [[BranchCoverageAnnotation]] [[ConditionalCoverageAnnotation]] [[StructuralCoverageAnnotation]] [[UserCoverageAnnotation]]
+  * @param chiselTestExceptions @todo remove this.
+  */
 class ChiselTestOptions private[stage] (
   val circuit:               Option[firrtl.ir.Circuit] = None,
   val topName:               Option[String] = None,
