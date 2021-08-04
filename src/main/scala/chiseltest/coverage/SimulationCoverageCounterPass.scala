cd0deeac83acaaa9c867fc1c7e15529cec51849d
Kevin Läufer <laeufer@cs.berkeley.edu>
2021-08-04 08:46:41 -0700

    coverage: turn cover statements into counters for simulation

---
+++ src/main/scala/chiseltest/coverage/SimulationCoverageCounterPass.scala
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package chiseltest.coverage
+
+import firrtl._
+import firrtl.annotations._
+import firrtl.stage.Forms
+import scala.collection.mutable
+
+/** This pass replaces each `cover` statement in the circuit with a register of the same name.
+  * The register will be incremented whenever the predicate and enable conditions are true.
+  * The register needs to be reset to zero by the simulator at the beginning of simulation and
+  * can then be read in order to get the cover count.
+  */
+object SimulationCoverageCounterPass extends Transform with DependencyAPIMigration {
+  override def prerequisites = Forms.LowForm
+
+  val CounterWidth = 32
+
+  override protected def execute(state: CircuitState): CircuitState = {
+    val c = CircuitTarget(state.circuit.main)
+    val modulesAndAnnos = state.circuit.modules.map(onModule(c, _))
+    val newAnnos = modulesAndAnnos.flatMap(_._2)
+    val circuit = state.circuit.copy(modules = modulesAndAnnos.map(_._1))
+    state.copy(circuit = circuit, annotations = newAnnos ++: state.annotations)
+  }
+
+  private def onModule(c: CircuitTarget, dm: ir.DefModule): (ir.DefModule, AnnotationSeq) = dm match {
+    case mod: ir.Module =>
+      val m = c.module(mod.name)
+      val annos = mutable.ListBuffer[Annotation]()
+      (mod.mapStmt(onStmt(m, annos, _)), annos.toList)
+    case other => (other, List())
+  }
+
+  private def onStmt(m: ModuleTarget, annos: mutable.ListBuffer[Annotation], s: ir.Statement): ir.Statement = s match {
+    case v: ir.Verification if v.op == ir.Formal.Cover => makeCoverCounter(m, annos, v)
+    case other => other.mapStmt(onStmt(m, annos, _))
+  }
+
+  private def makeCoverCounter(
+    m:     ModuleTarget,
+    annos: mutable.ListBuffer[Annotation],
+    cover: ir.Verification
+  ): ir.Statement = {
+    val tpe = ir.UIntType(ir.IntWidth(CounterWidth))
+    val ref = ir.Reference(cover.name, tpe, RegKind, SourceFlow)
+    val reg = ir.DefRegister(cover.info, cover.name, tpe, cover.clk, Utils.False(), ref)
+    val active = Utils.and(cover.en, cover.pred)
+    val plusOne = ir.DoPrim(
+      PrimOps.Add,
+      List(ref, ir.UIntLiteral(1, ir.IntWidth(CounterWidth))),
+      List(),
+      ir.UIntType(ir.IntWidth(CounterWidth + 1))
+    )
+    val next = ir.Mux(active, ir.DoPrim(PrimOps.Bits, List(plusOne), List(CounterWidth - 1, 0), tpe), ref)
+    val con = ir.Connect(cover.info, ref.copy(flow = SinkFlow), next)
+    annos.append(CoverCounterAnnotation(m.ref(cover.name)))
+    ir.Block(reg, con)
+  }
+}
+
+case class CoverCounterAnnotation(target: ReferenceTarget) extends SingleTargetAnnotation[ReferenceTarget] {
+  override def duplicate(n: ReferenceTarget) = copy(target = n)
+}
