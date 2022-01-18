d11aa78d7d2fdd39f9e679e7245bd30a5f00f5eb
Kevin Läufer <laeufer@cs.berkeley.edu>
2022-01-18 13:32:08 -0800

    test: add fixed point tests for chiseltest

--- src/test/scala/chiseltest/tests/IntervalTests.scala
+++ src/test/scala/chiseltest/tests/IntervalTests.scala
@@ -5,11 +5,10 @@ package chiseltest.tests
 import chisel3._
 import chisel3.experimental.Interval
 import chiseltest._
-import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.freespec.AnyFreeSpec
 
-class IntervalTests extends AnyFlatSpec with ChiselScalatestTester {
-  behavior of "Interval"
-  it should "interval reduce should work with BigDecimal" in {
+class IntervalTests extends AnyFreeSpec with ChiselScalatestTester {
+  "interval reduce should work with BigDecimal" in {
     test(new IntervalReduce(Interval(70.W, 60.BP), 10)) { dut =>
       val nums = (0 until dut.size).map { _ => 0.1 }
       nums.zipWithIndex.foreach { case (num, index) =>
@@ -21,7 +20,7 @@ class IntervalTests extends AnyFlatSpec with ChiselScalatestTester {
     }
   }
 
-  it should "interval reduce should fail without BigDecimal" in {
+  "interval reduce should fail without BigDecimal" in {
     val e = intercept[ChiselException] {
       test(new IntervalReduce(Interval(70.W, 60.BP), 10)) { dut =>
         val nums = (0 until dut.size).map { _ => 0.1 }
@@ -34,6 +33,23 @@ class IntervalTests extends AnyFlatSpec with ChiselScalatestTester {
       }
     }
     assert(e.getMessage.contains("is too big, precision lost with > 53 bits"))
+  }
+
+  private def testIntervalDivide(dut: IntervalDivide): Unit = {
+    for(d <- BigDecimal(0.0) to BigDecimal(15.0) by BigDecimal(1.0 / 3.0)) {
+      dut.in.poke(d.toDouble)
+      dut.clock.step()
+      val _ = dut.out.peekDouble()
+      dut.out.expect(d.toDouble / 4.0)
+    }
+  }
+
+  "with enough bits interval pseudo divide should work" in {
+    test(new IntervalDivide(Interval(64.W, 32.BP), 2)) (testIntervalDivide)
+  }
+
+  "not enough bits and interval pseudo divide will not work" in {
+    test(new IntervalDivide(Interval(10.W, 4.BP), 2)) (testIntervalDivide)
   }
 }
 
