e0c225dcc18a1635f1ce20b4c987fdaa8dc32f1b
Kevin Läufer <laeufer@cs.berkeley.edu>
2022-12-16 15:35:22 -0500

    partialPoke/Expect: add more tests, improve implementation

--- src/main/scala/chiseltest/exceptions.scala
+++ src/main/scala/chiseltest/exceptions.scala
@@ -25,3 +25,14 @@ class StopException(message: String) extends Exception(message)
 
 /** Indicates that a Chisel `assert(...)` or `assume(...)` statement has failed. */
 class ChiselAssertionError(message: String) extends Exception(message)
+
+/** Indicates that a value used in a poke/expect is not a literal.
+  * It could be hardware or a DontCare which is only allowed when using pokePartial/expectPartial.
+  */
+class NonLiteralValueError(val value: chisel3.Data, val signal: chisel3.Data, op: String)
+    extends Exception(
+      s"""Value $value for entry $signal is not a literal value!
+         |You need to fully specify all fields/entries when using $op.
+         |Maybe try using `${op}Partial` if you only want to use incomplete Vec/Bundle literals.
+         |""".stripMargin
+    )
