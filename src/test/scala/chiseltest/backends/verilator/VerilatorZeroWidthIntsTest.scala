cc7ba49fb44c7d51b1ca5bebd83a49fff988e200
Kevin Laeufer <laeufer@cs.berkeley.edu>
2023-01-19 00:25:49 -0500

    [test] use real 0-bit integer literals instead of 1-bit literals (#615)

--- src/test/scala/chiseltest/backends/verilator/VerilatorZeroWidthIntsTest.scala
+++ src/test/scala/chiseltest/backends/verilator/VerilatorZeroWidthIntsTest.scala
@@ -12,6 +12,9 @@ class VerilatorZeroWidthIntsTest extends AnyFreeSpec with ChiselScalatestTester 
 
   "peek, poke and expect zero width UInt" taggedAs RequiresVerilator  in {
     test(new PassthroughModule(UInt(0.W))).withAnnotations(annos) { c =>
+      c.in.poke(0.U(0.W))
+      c.in.expect(0.U(0.W))
+      // poking a `0.U` aka `0.U(1.W)` is also fine
       c.in.poke(0.U)
       c.in.expect(0.U)
       assert(c.in.peekInt() == 0)
@@ -21,6 +24,9 @@ class VerilatorZeroWidthIntsTest extends AnyFreeSpec with ChiselScalatestTester 
 
   "peek, poke and expect zero width SInt" taggedAs RequiresVerilator  in {
     test(new PassthroughModule(SInt(0.W))).withAnnotations(annos) { c =>
+      c.in.poke(0.S(0.W))
+      c.in.expect(0.S(0.W))
+      // poking a `0.S` aka `0.S(1.W)` is also fine
       c.in.poke(0.S)
       c.in.expect(0.S)
       assert(c.in.peekInt() == 0)
