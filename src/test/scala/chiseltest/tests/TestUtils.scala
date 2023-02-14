fed4822cc562293e373ed568d6a12139b631e94c
Kevin Laeufer <laeufer@cs.berkeley.edu>
2023-02-14 11:37:13 -0500

    Address chisel3 changes to DataMirror, AutoCloneType and new deprecation warnings (#619)

--- src/test/scala/chiseltest/tests/TestUtils.scala
+++ src/test/scala/chiseltest/tests/TestUtils.scala
@@ -3,8 +3,6 @@
 package chiseltest.tests
 
 import chisel3._
-import chisel3.experimental.{AutoCloneType, DataMirror}
-import chisel3.internal.requireIsChiselType
 import chisel3.util._
 
 import scala.collection.immutable.ListMap
@@ -43,7 +41,7 @@ class QueueModule[T <: Data](ioType: T, entries: Int) extends Module {
 }
 
 /** borrowed from chiselTests/RecordSpec.scala */
-final class CustomBundle(elts: (String, Data)*) extends Record with AutoCloneType {
+final class CustomBundle(elts: (String, Data)*) extends Record {
   val elements = ListMap(elts.map { case (field, elt) => field -> elt.cloneType }: _*)
   def apply(elt: String): Data = elements(elt)
 }
