2c3f4fc7ad3a9c7b983b9d9334d3fc985400b766
Kevin Läufer <laeufer@cs.berkeley.edu>
2023-06-09 13:33:03 -0400

    more tests passing

--- src/main/scala/chiseltest/simulator/ChiselBridge.scala
+++ src/main/scala/chiseltest/simulator/ChiselBridge.scala
@@ -3,12 +3,18 @@ package chiseltest.simulator
 import chisel3.RawModule
 import chisel3.stage._
 import chisel3.stage.phases._
+import chisel3.experimental.EnumAnnotations.{EnumComponentAnnotation, EnumDefAnnotation}
+import firrtl.transforms.{DontTouchAnnotation, NoDedupAnnotation}
 // this imports the [[firrtl]] package from Chisel (not to be confused with the firrtl2 compiler!
 import firrtl._
 import firrtl.ir._
-import firrtl.annotations.Annotation
+import firrtl.annotations._
 import firrtl.stage.FirrtlCircuitAnnotation
+import firrtl.passes.wiring.{SinkAnnotation, SourceAnnotation}
 import logger.LogLevelAnnotation
+
+/// Indicates that an unsupported Chisel annotation was encountered
+case class UnsupportedAnnotation(name: String, instance: String) extends firrtl2.annotations.NoTargetAnnotation
 
 /// wraps Chisel elaboration to bridge it over into the firrtl2 world
 private object ChiselBridge {
@@ -37,14 +43,10 @@ private object ChiselBridge {
     (stateWithUserAnnos, dut)
   }
 
-  private def toFirrtl2Anno(anno: Annotation): firrtl2.annotations.Annotation = anno match {
-    case _ => throw new NotImplementedError(s"TODO: convert ${anno}")
-  }
-
   private def annosToState(annos: AnnotationSeq): firrtl2.CircuitState = {
     val circuit = annos.collectFirst { case FirrtlCircuitAnnotation(c) => c }.get
     val filteredAnnos = annos.filterNot(isInternalAnno)
-    val firrtl2Annos = filteredAnnos.map(toFirrtl2Anno)
+    val firrtl2Annos = filteredAnnos.map(convert)
     val firrtl2Circuit = convert(circuit)
     firrtl2.CircuitState(firrtl2Circuit, firrtl2Annos)
   }
@@ -54,6 +56,29 @@ private object ChiselBridge {
         _: EmittedCircuitAnnotation[_] | _: LogLevelAnnotation =>
       true
     case _ => false
+  }
+
+  private def convert(m: ModuleTarget): firrtl2.annotations.ModuleTarget =
+    firrtl2.annotations.ModuleTarget(m.circuit, m.module)
+  private def convert(c: ComponentName): firrtl2.annotations.ComponentName =
+    firrtl2.annotations.ComponentName(c.name, convert(c.module))
+  private def convert(c:      CircuitName): firrtl2.annotations.CircuitName = firrtl2.annotations.CircuitName(c.name)
+  private def convertNamed(n: Named): firrtl2.annotations.Named = n match {
+    case target: Target      => ???
+    case c:      CircuitName => convert(c)
+    case ModuleName(name, circuit) => firrtl2.annotations.ModuleName(name, convert(circuit))
+    case c: ComponentName => convert(c)
+  }
+  private def convert(anno: Annotation): firrtl2.annotations.Annotation = anno match {
+    case firrtl.transforms.BlackBoxInlineAnno(target, name, text) =>
+      firrtl2.transforms.BlackBoxInlineAnno(convert(target), name, text)
+    case SourceAnnotation(target, pin) => firrtl2.passes.wiring.SourceAnnotation(convert(target), pin)
+    case SinkAnnotation(target, pin)   => firrtl2.passes.wiring.SinkAnnotation(convertNamed(target), pin)
+    case DontTouchAnnotation(target)   => firrtl2.transforms.DontTouchAnnotation(convert(target))
+    case NoDedupAnnotation(target)     => firrtl2.transforms.NoDedupAnnotation(convert(target))
+    case a: EnumComponentAnnotation => UnsupportedAnnotation("EnumComponentAnnotation", a.toString)
+    case a: EnumDefAnnotation       => UnsupportedAnnotation("EnumDefAnnotation", a.toString)
+    case _ => throw new NotImplementedError(s"TODO: convert ${anno}")
   }
 
   private def convert(c: Circuit): firrtl2.ir.Circuit =
@@ -107,6 +132,22 @@ private object ChiselBridge {
   }
   private def convert(p: Port): firrtl2.ir.Port =
     firrtl2.ir.Port(convert(p.info), p.name, convert(p.direction), convert(p.tpe))
+  private def convert(op: Formal.Value): firrtl2.ir.Formal.Value = op match {
+    case firrtl.ir.Formal.Assert => firrtl2.ir.Formal.Assert
+    case firrtl.ir.Formal.Assume => firrtl2.ir.Formal.Assume
+    case firrtl.ir.Formal.Cover  => firrtl2.ir.Formal.Cover
+  }
+  private def convert(d: MPortDir): firrtl2.MPortDir = d match {
+    case MInfer     => firrtl2.MInfer
+    case MRead      => firrtl2.MRead
+    case MReadWrite => firrtl2.MReadWrite
+    case MWrite     => firrtl2.MWrite
+  }
+  private def convertReadUnderWrite(r: ReadUnderWrite.Value): firrtl2.ir.ReadUnderWrite.Value = r match {
+    case firrtl.ir.ReadUnderWrite.Undefined => firrtl2.ir.ReadUnderWrite.Undefined
+    case firrtl.ir.ReadUnderWrite.Old       => firrtl2.ir.ReadUnderWrite.Old
+    case firrtl.ir.ReadUnderWrite.New       => firrtl2.ir.ReadUnderWrite.New
+  }
   private def convert(s: Statement): firrtl2.ir.Statement = s match {
     case DefNode(info, name, value) => firrtl2.ir.DefNode(convert(info), name, convert(value))
     case Connect(info, loc, expr)   => firrtl2.ir.Connect(convert(info), convert(loc), convert(expr))
@@ -120,7 +161,16 @@ private object ChiselBridge {
     case DefInstance(info, name, module, tpe) => firrtl2.ir.DefInstance(convert(info), name, module, convert(tpe))
     case PartialConnect(info, loc, expr)      => firrtl2.ir.PartialConnect(convert(info), convert(loc), convert(expr))
     case Attach(info, exprs)                  => firrtl2.ir.Attach(convert(info), exprs.map(convert))
-
+    case Stop(info, ret, clk, en)             => firrtl2.ir.Stop(convert(info), ret, convert(clk), convert(en))
+    case Print(info, string, args, clk, en) =>
+      firrtl2.ir.Print(convert(info), convert(string), args.map(convert), convert(clk), convert(en))
+    case Verification(op, info, clk, pred, en, msg) =>
+      firrtl2.ir.Verification(convert(op), convert(info), convert(clk), convert(pred), convert(en), convert(msg))
+    case IsInvalid(info, expr) => firrtl2.ir.IsInvalid(convert(info), convert(expr))
+    case CDefMemory(info, name, tpe, size, seq, readUnderWrite) =>
+      firrtl2.CDefMemory(convert(info), name, convert(tpe), size, seq, convertReadUnderWrite(readUnderWrite))
+    case CDefMPort(info, name, tpe, mem, exps, direction) =>
+      firrtl2.CDefMPort(convert(info), name, convert(tpe), mem, exps.map(convert), convert(direction))
     case other => throw new NotImplementedError(s"TODO: convert ${other}")
   }
   private def convert(op: PrimOp): firrtl2.ir.PrimOp = op match {
@@ -166,6 +216,7 @@ private object ChiselBridge {
     case SubAccess(expr, index, tpe)   => firrtl2.ir.SubAccess(convert(expr), convert(index), convert(tpe))
     case SubIndex(expr, value, tpe)    => firrtl2.ir.SubIndex(convert(expr), value, convert(tpe))
     case DoPrim(op, args, consts, tpe) => firrtl2.ir.DoPrim(convert(op), args.map(convert), consts, convert(tpe))
+    case Mux(cond, tval, fval, tpe)    => firrtl2.ir.Mux(convert(cond), convert(tval), convert(fval), convert(tpe))
     case other                         => throw new NotImplementedError(s"TODO: convert ${other}")
   }
 }
