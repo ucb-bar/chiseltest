46934dea6ab0ba1064e5a19513b6a9d54a03c776
Aleksandar Kondić <konda@uni.kg.ac.rs>
2023-08-21 19:00:47 +0200

    Replace getDataNames with DataMirror.fullModulePorts in PeekPokeTester

--- src/test/scala/chiseltest/tests/OpaqueTypeTest.scala
+++ src/test/scala/chiseltest/tests/OpaqueTypeTest.scala
@@ -6,6 +6,7 @@ import chisel3.experimental.BundleLiterals.AddBundleLiteralConstructor
 import chisel3.experimental.OpaqueType
 import chisel3._
 import chiseltest._
+import chiseltest.iotesters.PeekPokeTester
 import org.scalatest.flatspec.AnyFlatSpec
 
 import scala.collection.immutable.SeqMap
@@ -30,12 +31,27 @@ class OpaqueTypeTest extends AnyFlatSpec with ChiselScalatestTester {
       dut.out.expect(rec(_val))
     }
 
+  class PokeExpectTester[T <: Data](dut: OpaquePassthrough[T], _val: => T) extends PeekPokeTester(dut) {
+    poke(dut.in, IndexedSeq(_val.litValue))
+    expect(dut.out, IndexedSeq(_val.litValue))
+  }
+
+  def testPokeExpectTester[T <: Data](_val: => T): Unit =
+    test(new OpaquePassthrough(_val.cloneType))
+      .runPeekPoke(new PokeExpectTester(_, _val))
+
   behavior of "OpaqueType"
 
   it should "poke and expect successfully" in {
     testPokeExpect(4.U(6.W))
     testPokeExpect(-4.S(8.W))
     testPokeExpect(rec(5.U(3.W)))
+  }
+
+  it should "poke and expect successfully using PeekPokeTester" in {
+    testPokeExpectTester(4.U(6.W))
+    testPokeExpectTester(-4.S(8.W))
+    testPokeExpectTester(rec(5.U(3.W)))
   }
 
 }
