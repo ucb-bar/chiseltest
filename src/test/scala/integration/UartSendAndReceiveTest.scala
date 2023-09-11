936599771ffbde9afe58d2551a492678a5487d18
Kevin Laeufer <laeufer@cs.berkeley.edu>
2023-09-11 12:49:29 -0400

    [integration] add more uart benchmarks (#681)

--- src/test/scala/integration/UartSendAndReceiveTest.scala
+++ src/test/scala/integration/UartSendAndReceiveTest.scala
@@ -3,6 +3,7 @@ package integration
 import chisel3._
 import chisel3.util.ShiftRegister
 import chiseltest._
+import chiseltest.internal.NoThreadingAnnotation
 import org.scalatest.freespec.AnyFreeSpec
 
 class UartPassThrough extends Module {
@@ -35,6 +36,18 @@ class UartSendAndReceiveTest extends AnyFreeSpec with ChiselScalatestTester {
       val expected = BigInt(Bits, rnd)
       val actual = receive(dut.clock, dut.tx, bitDelay)
       assert(expected == actual)
+    }
+  }
+
+  private def runFsmTest(dut: UartPassThrough, n: Int, bitDelay: Int): Unit = {
+    dut.rx.poke(true.B)
+    val rx = new UartRxStateMachine(dut.tx, n, bitDelay, Bits)
+    val tx = new UartTxStateMachine(dut.rx, n, bitDelay, Bits)
+    def allDone = rx.done && tx.done
+    while (!allDone) {
+      tx.step()
+      rx.step()
+      dut.clock.step()
     }
   }
 
@@ -72,15 +85,121 @@ class UartSendAndReceiveTest extends AnyFreeSpec with ChiselScalatestTester {
     value
   }
 
-  private def runTimedTest(dut: UartPassThrough, n: Int, bitDelay: Int): Unit = {
+  private def runTimedTest(dut: UartPassThrough, n: Int, bitDelay: Int, fsm: Boolean): Unit = {
+    val (foo, kind) = if (fsm) {
+      (() => runFsmTest(dut, n, bitDelay), "FSM")
+    } else {
+      (() => runTest(dut, n, bitDelay), "chiseltest")
+    }
+    // warmup
+    foo()
     val start = System.nanoTime()
-    runTest(dut, n, bitDelay)
+    foo()
     val delta = System.nanoTime() - start
-    println(s"Took ${delta / 1000.0 / 1000.0 / 1000.0}s to run $n UART transactions with bitDelay=$bitDelay.")
+    println(
+      s"Took ${delta / 1000.0 / 1000.0 / 1000.0}s to run $n UART transactions with bitDelay=$bitDelay and $kind threading."
+    )
   }
 
-  "run 100 uart transactions in software" in {
-    test(new UartPassThrough)(runTimedTest(_, 100, bitDelay = 50))
+  "run 1000 uart transactions with bitDelay=1 in software" in {
+    test(new UartPassThrough)(runTimedTest(_, 1000, bitDelay = 1, fsm = false))
   }
 
+  "run 100 uart transactions with bitDelay=50 in software" in {
+    test(new UartPassThrough)(runTimedTest(_, 100, bitDelay = 50, fsm = false))
+  }
+
+  "run 1000 uart transactions with bitDelay=1 in software with manual FSM" in {
+    test(new UartPassThrough)
+      .withAnnotations(Seq(NoThreadingAnnotation))(runTimedTest(_, 1000, bitDelay = 1, fsm = true))
+  }
+
+  "run 100 uart transactions with bitDelay=50 in software with manual FSM" in {
+    test(new UartPassThrough)
+      .withAnnotations(Seq(NoThreadingAnnotation))(runTimedTest(_, 100, bitDelay = 50, fsm = true))
+  }
+
+}
+
+/** The send thread from above, manually turned into a state machine */
+
+class UartTxStateMachine(tx: UInt, n: Int, bitDelay: Int, Bits: Int) {
+  private val Debug = false
+  private var state = 0
+  private var delay_count = 0
+  private var value = BigInt(0)
+  private var txCount = 0
+  val rnd = new scala.util.Random(1234)
+
+  def done: Boolean = txCount == n && delay_count == 0 && state == 0
+  def step(): Unit = {
+    if (Debug) println(s"TX: delay_count=$delay_count, state=$state, txCount=$txCount")
+    if (delay_count > 0) {
+      delay_count -= 1
+    } else {
+      state = if (state == 0) {
+        if (txCount < n) {
+          value = BigInt(Bits, rnd)
+          txCount += 1
+          // Start bit
+          tx.poke(0)
+          delay_count = bitDelay - 1
+          1
+        } else {
+          0
+        }
+      } else if (state >= 1 && state < 9) {
+        val bit = (value >> (state - 1)) & 1
+        tx.poke(bit)
+        delay_count = bitDelay - 1
+        state + 1
+      } else {
+        assert(state == 9)
+        // Stop bit
+        tx.poke(1)
+        delay_count = bitDelay - 1
+        0
+      }
+    }
+  }
+}
+
+/** The receive thread from above, manually turned into a state machine */
+
+class UartRxStateMachine(rx: UInt, n: Int, bitDelay: Int, Bits: Int) {
+  private val Debug = false
+  private var state = 0
+  private var delay_count = 0
+  private var byte = BigInt(0)
+  private var rxCount = 0
+  val rnd = new scala.util.Random(1234)
+  def done: Boolean = rxCount == n && delay_count == 0 && state == 0
+  def step(): Unit = {
+    if (Debug) println(s"RX: delay_count=$delay_count, state=$state, rxCount=$rxCount")
+    if (delay_count > 0) {
+      delay_count -= 1
+    } else {
+      state = if (state == 0) {
+        if (rx.peekInt() == 0) {
+          delay_count = bitDelay - 1
+          byte = 0
+          1
+        } else {
+          0
+        }
+      } else if (state >= 1 && state < 9) {
+        byte = (rx.peekInt() << (state - 1)) | byte
+        delay_count = bitDelay - 1
+        state + 1
+      } else {
+        assert(state == 9)
+        // check result
+        rxCount += 1
+        val expected = BigInt(Bits, rnd)
+        assert(byte == expected)
+        assert(rx.peekInt() == 1, "stop bit")
+        0
+      }
+    }
+  }
 }
