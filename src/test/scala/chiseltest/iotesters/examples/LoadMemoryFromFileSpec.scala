296ed6cea4cac698fd9a7185f6f883172b4f6d05
Nikita Bloshchanevich <nikblos@outlook.com>
2023-11-16 16:57:17 +0100

    Bugfix: loadMemoryFromFile support absolute paths
    Previously, specifying an absolute path in loadMemoryFromFileInline
    would cause an error when running with treadle, because the path was
    absolutized using os.path.RelPath. The latter function throws an
    exception for absolute paths.
    
    Now both absolute and relative paths work (matching the behaviour of
    Verilator).
    
    Note that this has no security implications, since the previous
    "relative-path" based implementation also allowed "../../" paths;
    existing relative paths should not be affected.

--- src/test/scala/chiseltest/iotesters/examples/LoadMemoryFromFileSpec.scala
+++ src/test/scala/chiseltest/iotesters/examples/LoadMemoryFromFileSpec.scala
@@ -10,7 +10,7 @@ import chiseltest.simulator.RequiresVerilator
 import firrtl2.options.TargetDirAnnotation
 import org.scalatest.freespec.AnyFreeSpec
 
-class UsesMem(memoryDepth: Int, memoryType: Bits) extends Module {
+class UsesMem(memoryDepth: Int, memoryType: Bits, fileName: String) extends Module {
   val io = IO(new Bundle {
     val address = Input(UInt(memoryType.getWidth.W))
     val value = Output(memoryType)
@@ -19,7 +19,7 @@ class UsesMem(memoryDepth: Int, memoryType: Bits) extends Module {
 
   val memory = Mem(memoryDepth, memoryType)
 
-  loadMemoryFromFileInline(memory, "src/test/resources/iotesters/mem1.txt")
+  loadMemoryFromFileInline(memory, fileName)
 
   io.value := memory(io.address)
 
@@ -27,6 +27,9 @@ class UsesMem(memoryDepth: Int, memoryType: Bits) extends Module {
 
   low.io.address := io.address
   io.value2 := low.io.value
+}
+object UsesMem {
+  val MEM1 = "src/test/resources/iotesters/mem1.txt"
 }
 
 class UsesMemLow(memoryDepth: Int, memoryType: Data) extends Module {
@@ -56,8 +59,21 @@ class LoadMemoryFromFileSpec extends AnyFreeSpec with ChiselScalatestTester {
   "Users can specify a source file to load memory from" taggedAs RequiresVerilator in {
 
     val targetDir = TargetDirAnnotation("test_run_dir/load_mem_test")
-    test(new UsesMem(memoryDepth = 8, memoryType = UInt(16.W)))
+    test(new UsesMem(memoryDepth = 8, memoryType = UInt(16.W), "src/test/resources/iotesters/mem1.txt"))
       .withAnnotations(Seq(VerilatorBackendAnnotation, targetDir))
+      .runPeekPoke(new LoadMemoryFromFileTester(_))
+  }
+
+  "Treadle supports loadFromFileInline using absolute paths" in {
+    // An absolute path
+    val path: os.Path = os.pwd / os.RelPath(UsesMem.MEM1)
+    test(new UsesMem(memoryDepth = 8, memoryType = UInt(16.W), path.toString()))
+      .runPeekPoke(new LoadMemoryFromFileTester(_))
+  }
+
+  "Treadle also supports loadFromFileInline using relative paths" in {
+    // An absolute path
+    test(new UsesMem(memoryDepth = 8, memoryType = UInt(16.W), UsesMem.MEM1))
       .runPeekPoke(new LoadMemoryFromFileTester(_))
   }
 }
